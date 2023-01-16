83cfc64b9b7525474de8dcfdcca05b535f32ac3c
Artur Kosinski <arturkosinsku@outlook.com>
2023-01-16 15:34:08 +0200

    Creation of blanks for all components Todo

---
+++ src/components/Todos/TodoForm.js
@@ -0,0 +1,10 @@
+const TodoForm = () => {
+  return (
+    <form>
+      <input placeholder="Enter new todo"></input>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default TodoForm;
